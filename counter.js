bfede68f7f0083dbd8bd2de3bac5cf67f2fbc4d7
syedmanzoorul <101462789+syedmanzoorul@users.noreply.github.com>
2022-03-15 19:26:18 +0600

    counter program

---
+++ counter.js
@@ -0,0 +1,34 @@
+
+let a = 0;
+
+function h3(){
+    a = a + 1;
+    document.getElementById("h1").innerHTML = a;
+    
+}
+function h4(){
+    a = a - 1;
+    document.getElementById("h1").innerHTML = a;
+    
+}
+function h5(){
+    a = a * 2;
+    document.getElementById("h1").innerHTML = a;
+    
+}
+function h6(){
+    a = a / 2;
+    document.getElementById("h1").innerHTML = a;
+    
+}
+function h7(){
+    a = a + 100;
+    document.getElementById("h1").innerHTML = a;
+    
+}
+function h8(){
+    a = a + " + "
+    document.getElementById("co").textContent += a ;
+    a = 0
+
+}
